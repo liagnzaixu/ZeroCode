49748b077e585657abfab9f42408e270c9ba28f8
liangzaixu <280681432@qq.com>
2017-12-01 16:18:34 +0800

    提取uoload的脚本至单独的.js文件

--- ZeroCode.WebUI/App_Start/BundleConfig.cs
+++ ZeroCode.WebUI/App_Start/BundleConfig.cs
@@ -19,6 +19,8 @@ namespace ZeroCode.WebUI
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js","~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/bootstrap.css","~/Content/site.css"));
+
+            //BundleTable.EnableOptimizations = true;
         }
     }
 }
